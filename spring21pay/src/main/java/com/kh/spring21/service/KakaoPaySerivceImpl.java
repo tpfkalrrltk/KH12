7f954951d304886fca7d7836f7c2f5d8d11b1a3f
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-10-24 16:29:04 +0900

    카카오페이 승인 서비스 구현 중

--- spring21pay/src/main/java/com/kh/spring21/service/KakaoPaySerivceImpl.java
+++ spring21pay/src/main/java/com/kh/spring21/service/KakaoPaySerivceImpl.java
@@ -2,6 +2,7 @@ package com.kh.spring21.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
 
 import javax.servlet.Servlet;
 
@@ -15,6 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.kh.spring21.configuration.KakaoPayProperties;
+import com.kh.spring21.vo.KakaoPayApproveResponseVO;
 import com.kh.spring21.vo.KakaoPayReadyRequestVO;
 import com.kh.spring21.vo.KakaoPayReadyResponseVO;
 
@@ -56,6 +58,24 @@ public class KakaoPaySerivceImpl implements KakaoPayService {
 		HttpEntity entity = new HttpEntity(body, headers);
 
 		KakaoPayReadyResponseVO response = template.postForObject(uri, entity, KakaoPayReadyResponseVO.class);
+
+		return response;
+	}
+
+	@Override
+	public KakaoPayApproveResponseVO approve(KakaoPayApproveResponseVO request) throws URISyntaxException {
+		
+		URI uri = new URI("https://kapi.kakao.com/v1/payment/approve");
+		
+		MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+		body.add("cid", kakaoPayProperties.getCid());
+		body.add("tid", "T537691951b66fd4faa6");
+		body.add("partner_order_id", "fb76295f-5acd-4d6a-988a-ef9901320968");
+		body.add("partner_user_id", "testuser1");
+		body.add("pg_token", "9ae0214215fbc540b2c6");
+		
+		HttpEntity entity =new HttpEntity(body,headers);
+		KakaoPayApproveResponseVO response = template.postForObject(uri, entity, KakaoPayApproveResponseVO.class);
 
 		return response;
 	}
--- spring21pay/src/main/java/com/kh/spring21/service/KakaoPayService.java
+++ spring21pay/src/main/java/com/kh/spring21/service/KakaoPayService.java
@@ -2,11 +2,14 @@ package com.kh.spring21.service;
 
 import java.net.URISyntaxException;
 
+import com.kh.spring21.vo.KakaoPayApproveResponseVO;
 import com.kh.spring21.vo.KakaoPayReadyRequestVO;
 import com.kh.spring21.vo.KakaoPayReadyResponseVO;
 
 
 public interface KakaoPayService {
 	KakaoPayReadyResponseVO ready(KakaoPayReadyRequestVO request) throws URISyntaxException;
-
+	KakaoPayApproveResponseVO approve(KakaoPayApproveResponseVO request) throws URISyntaxException;
+//	KakaoPayDetailResponseVO detail(KakaoPayDetailResponseVO request) throws URISyntaxException;
+//	KakaoPayCancelResponseVO detail(KakaoPayCancelResponseVO request) throws URISyntaxException;
 }
