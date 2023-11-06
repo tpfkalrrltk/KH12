15ad41efd14e8f79b880c140b72848903b9a559a
tpfkalrrltk <tpfkalrrltk@naver.com>
2023-11-06 15:14:10 +0900

    @JsonIgnore

--- spring22rest/src/main/java/com/kh/spring22/dto/PocketmonDto.java
+++ spring22rest/src/main/java/com/kh/spring22/dto/PocketmonDto.java
@@ -2,6 +2,8 @@ package com.kh.spring22.dto;
 
 import javax.validation.constraints.Pattern;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -25,6 +27,7 @@ public class PocketmonDto {
 	@Schema(description = "몬스터 타입", allowableValues = {"풀","불","전기","무속성"}, nullable = false, example = "전기")
 	private String type;
 
+	@JsonIgnore //JSON 생성 시 이 항목은 제외 시켜라!
 	@Schema(hidden = true)
 	public boolean isEmpty() {
 		return no == 0 && name == null && type == null;
